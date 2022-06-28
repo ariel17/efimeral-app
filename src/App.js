f9d1eca87fbce0c05300544818f42847d94f665e
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-06-28 09:46:18 -0300

    Adds navbar with dummy text.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BoxLoading } from 'react-loadingg';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        THIS IS THE HEADER
-      </header>
-      <BoxLoading />;
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            React Bootstrap
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Link 1</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <BoxLoading />
+    </>
   );
 }
 
